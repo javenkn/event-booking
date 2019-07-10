2d839e530a87f7cad14fc62e4c4f0776844227e6
javenkn <javenkn7@yahoo.com>
2019-07-10 19:25:52 +0900

    sets up graphql with a root query and mutation

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World!');
-});
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => ['Romantic Cooking', 'Sailing', 'All-night coding'],
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 app.listen(3000);
