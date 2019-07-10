9b3ca1aecc329220e35f1e3f23e890db321db86f
javenkn <javenkn7@yahoo.com>
2019-07-10 20:59:17 +0900

    Implements mongoose and mongodb with graphQL

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -42,20 +43,50 @@ app.use(
       }
     `),
     rootValue: {
-      events: () => events,
+      events: () =>
+        Event.find()
+          .then(events =>
+            events.map(event => ({
+              ...event._doc,
+              _id: event.id,
+            })),
+          )
+          .catch(err => {
+            throw err;
+          }),
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           ...args.eventInput,
           price: +args.eventInput.price,
-          date: new Date().toISOString(),
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   }),
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-dlpcs.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
