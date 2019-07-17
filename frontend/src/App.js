36d2a213201596762f3a9d28fb7d6945795f831a
javenkn <javenkn7@yahoo.com>
2019-07-17 16:43:01 +0900

    creates and styles a main navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -10,12 +11,17 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Switch>
-          <Redirect from='/' to='/auth' exact />
-          <Route path='/auth' component={AuthPage} />
-          <Route path='/events' component={EventsPage} />
-          <Route path='/bookings' component={BookingsPage} />
-        </Switch>
+        <>
+          <MainNavigation />
+          <main className='main-content'>
+            <Switch>
+              <Redirect from='/' to='/auth' exact />
+              <Route path='/auth' component={AuthPage} />
+              <Route path='/events' component={EventsPage} />
+              <Route path='/bookings' component={BookingsPage} />
+            </Switch>
+          </main>
+        </>
       </BrowserRouter>
     </div>
   );
---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+
+export default function MainNavigation() {
+  return (
+    <header className='main-navigation'>
+      <div className='main-navigation__logo'>
+        <h1>Easy Event</h1>
+      </div>
+      <div className='main-navigation__items'>
+        <ul>
+          <li>
+            <NavLink to='/auth'>Authenticate</NavLink>
+          </li>
+          <li>
+            <NavLink to='/events'>Events</NavLink>
+          </li>
+          <li>
+            <NavLink to='/bookings'>Bookings</NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
