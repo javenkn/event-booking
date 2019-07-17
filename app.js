02b0bec09f301e1ba52d2d088a804e1c9d9846dd
javenkn <javenkn7@yahoo.com>
2019-07-17 18:17:21 +0900

    Styles up the authentication page and adds login/signup api

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -31,7 +41,7 @@ mongoose
     }?retryWrites=true&w=majority`,
   )
   .then(() => {
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch(err => {
     console.log(err);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+
+import './Auth.css';
 
 export default function AuthPage() {
-  return <h1>The Auth Page</h1>;
+  const [isLogin, setIsLogin] = useState(true);
+  const emailEl = useRef(null);
+  const passwordEl = useRef(null);
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const email = emailEl.current.value;
+    const password = passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestQuery = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      requestQuery = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `,
+      };
+    }
+    // request to the backend
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestQuery),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed.');
+        }
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <form className='auth-form' onSubmit={submitHandler}>
+      <div className='form-control'>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' ref={emailEl} />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' ref={passwordEl} />
+      </div>
+      <div className='form-actions'>
+        <button type='submit'>Submit</button>
+        <button type='button' onClick={() => setIsLogin(!isLogin)}>
+          Switch to {isLogin ? 'Signup' : 'Login'}
+        </button>
+      </div>
+    </form>
+  );
 }
